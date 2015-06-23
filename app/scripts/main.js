97ce3af47a604702a51ea52dd8dc5e9e932d6d59
Kim Wijk <kim.wijk@gmail.com>
2015-06-23 23:35:55 +0200

    Style updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,53 +24,6 @@
   var navdrawerContainer = querySelector('.navdrawer-container');
   var body = document.body;
   var appbarElement = querySelector('.app-bar');
-  var menuBtn = querySelector('.menu');
   var main = querySelector('main');
-
-  function closeMenu() {
-    body.classList.remove('open');
-    appbarElement.classList.remove('open');
-    navdrawerContainer.classList.remove('open');
-  }
-
-  function toggleMenu() {
-    body.classList.toggle('open');
-    appbarElement.classList.toggle('open');
-    navdrawerContainer.classList.toggle('open');
-    navdrawerContainer.classList.add('opened');
-  }
-
-  main.addEventListener('click', closeMenu);
-  menuBtn.addEventListener('click', toggleMenu);
-  navdrawerContainer.addEventListener('click', function (event) {
-    if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
-      closeMenu();
-    }
-  });
-})();
-
-(function() {
-
-  'use strict';
-
-  document.querySelector('.material-design-hamburger__icon').addEventListener(
-    'click',
-    function() {      
-      var child;
-      
-      document.body.classList.toggle('background--blur');
-      this.parentNode.nextElementSibling.classList.toggle('menu--on');
-
-      child = this.childNodes[1].classList;
-
-      if (child.contains('material-design-hamburger__icon--to-arrow')) {
-        child.remove('material-design-hamburger__icon--to-arrow');
-        child.add('material-design-hamburger__icon--from-arrow');
-      } else {
-        child.remove('material-design-hamburger__icon--from-arrow');
-        child.add('material-design-hamburger__icon--to-arrow');
-      }
-
-    });
 
 })();
